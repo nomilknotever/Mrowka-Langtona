95f8b01299207643cf03fb666997c6326798c2bd
nomilknotever <01187598@pw.edu.pl>
2024-01-22 22:32:45 +0100

    dziala

--- mrowka.c
+++ mrowka.c
@@ -1,78 +1,188 @@
+#include "mrowka.h"
 
-#include"mrowka.h"
+int main(int argc, char *argv[]) {
+    // 1-dlugosc X 2- dlugos Y 3- liczba iteracji 4-
+    ant mrowka;
 
-int main(int argc, char *argv[]){//1-dlugosc X 2- dlugos Y 3- liczba iteracji 4-
-ant mrowka;
-srand(time(NULL));                     //4- przedrostek 5- procent zapelnienia(int)6-poczatkowy zwrot
-                                            //1-gora2-prawo3-dol4-lewo
-                                            //6-zwrot
-int x = 5;
-int y = 5;
-printf("▶ \n");
-
-    char *c = "▶";
-    printf("%s %d \n", c, strlen(c));
-
-    // setlocale(LC_ALL, "");
-    setlocale(LC_ALL, "C.UTF-8");
-    wchar_t *wc = L"▶";
-    printf("%ls %d \n", wc, wcslen(wc));
-pole pola[x][y];
-char przedrostek[50] = "file";
-int iteracje = 5;
- for(int i = 0; i < y; i++){
-            for(int j = 0; j < x; j++){
-                pola[j][i].kolor=1;
-int opt;
+    srand(time(NULL)); // 4- przedrostek 5- procent zapelnienia(int)6-poczatkowy zwrot
+    // 1-gora2-prawo3-dol4-lewo
+    // 6-zwrot
+    int x = 5;
+    int y = 5;
+    char nazwaPliku[60];
+    int *pola;
+	int procent;
+    char przedrostek[50] = "";
+    int iteracje = 5;
+    /* for(int i = 0; i < y; i++){
+                for(int j = 0; j < x; j++){
+                    pola[j][i].kolor=1;*/
+    int opt, percentFlag = 0, fileFlag = 0;
     while ((opt = getopt(argc, argv, "x:y:i:z:n:p:")) != -1) {
         switch (opt) {
             case 'x':
-                x = atoi(optarg);
+                if (atoi(optarg) > 2 && atoi(optarg) < 80) {
+                    x = atoi(optarg);
+                } else {
+                    printf("Zostaly wpisane zle wymiary planszy, przyjeto szerokosc 5\n");
+                }
                 break;
+
             case 'y':
-                y = atoi(optarg);
+                if (atoi(optarg) > 2 && atoi(optarg) < 80) {
+                    y = atoi(optarg);
+                } else {
+                    printf("Zostaly wpisane zle wymiary planszy, przyjeto wysokosc 5\n");
+                }
                 break;
             case 'i':
                 iteracje = atoi(optarg);
-                break;  
+                break;
             case 'z':
-                if (atoi(optarg) == 1 || atoi(optarg) == 2 || atoi(optarg) == 3 || atoi(optarg) == 4){
+                if (atoi(optarg) == 1 || atoi(optarg) == 2 || atoi(optarg) == 3 || atoi(optarg) == 4) {
                     mrowka.zwrot = atoi(optarg);
-                
+
+                } else {
+                    printf("Wprowadź prawidłowy zwrot - 1-góra, 2-prawo, 3-dół lub 4-lewo. Został wpisany zły zwrot,przyjęto początkowy zwrot w górę\n");
                 }
-                else{fprintf(stderr, "Wprowadź prawidłowy zwrot - 1-góra, 2-prawo, 3-dół lub 4-lewo\n");
-                exit(EXIT_FAILURE);}
                 break;
             case 'n':
-		strcpy(przedrostek,optarg); 
+                if (strlen(optarg) < 1 || strlen(optarg) > 50) {
+                    printf("Podano zły przedrostek nazwy pliku - iteracje zostaną zapisane na standardowe wyjście\n");
+                    break;
+                }
+                strcpy(przedrostek, optarg);
+                fileFlag = 1;
+                break;
             case 'p':
-		int procent = atoi(optarg);
-		    for(int i = 0; i < y; i++){
-        		for(int j = 0; j < x; j++){
-            			if (rand() % 100 < procent){
-               	 			pola[j][i].kolor = 1;
-           	 }
-            	else{
-                	pola[j][i].kolor = 0;
-            	}
-        }
-        }
-            case '?':
-                fprintf(stderr, "Wpisz argumenty wywolania");
-                exit(EXIT_FAILURE);
+                if (atoi(optarg) < 101 && atoi(optarg) > -1) {
+                    percentFlag = 1;
+                    procent = atoi(optarg);
+
+                } else {
+                    printf("Podano zły procent - wszystkie pola na początku będą białe");
+                }
+                break;
         }
     }
 
+    pola = (int *)calloc(x * y, sizeof(int));
+    mrowka.currentX = x / 2;
+    mrowka.currentY = y / 2;
+	mrowka.zwrot = 1;
+    int dotychczasoweIteracje = 0;
+    int exitFlag = 0;
+    char mapa[3000];
+    if(percentFlag == 1){
+	 for(int q = 0; q < x*y;q++){
+    		if(rand() % 100 < procent){
+			pola[q] = 1;	
+	}else{pola[q]=0;}
+	 }
+	 
     }
+    else{
+    	for(int q = 0; q < x*y;q++){
+		pola[q]=0;
+	}
+    }
+    while (dotychczasoweIteracje < iteracje && exitFlag == 0) {
 
-}
-printStart(x); 
-int dotychczasoweIteracje = 0;
-while(dotychczasoweIteracje < iteracje){
+        createMap(mapa, x, y, mrowka, pola);
+        printf("%s", mapa);
 
+        if (pola[mrowka.currentX + mrowka.currentY * x] == 1) { // czarny - obrot w lewo
 
-}
-
-return 0;
+            switch (mrowka.zwrot) {
+                case 1:
+                    mrowka.zwrot = 4;
+                    if (mrowka.currentX > 0) {
+                        mrowka.currentX--;
+                    } else {
+                        exitFlag = -1;
+                    }
+                    break;
+                case 2:
+                    mrowka.zwrot = 1;
+                    if (mrowka.currentY > 0) {
+                        mrowka.currentY--;
+                    } else {
+                        exitFlag = -1;
+                    }
+                    break;
+                case 3:
+                    mrowka.zwrot = 2;
+                    if (mrowka.currentX < x - 1) {
+                        mrowka.currentX++;
+                    } else {
+                        exitFlag = -1;
+                    }
+                    break;
+                case 4:
+                    mrowka.zwrot = 3;
+                    if (mrowka.currentY < y - 1) {
+                        mrowka.currentY++;
+                    } else {
+                        exitFlag = -1;
+                    }
+                    break;
+            }
+        } else { // bialy - obrot w prawo
+            switch (mrowka.zwrot) {
+                case 1:
+                    mrowka.zwrot = 2;
+                    if (mrowka.currentX < x - 1) {
+                        mrowka.currentX++;
+                    } else {
+                        exitFlag = -1;
+                    }
+                    break;
+                case 2:
+                    mrowka.zwrot = 3;
+                    if (mrowka.currentY < y - 1) {
+                        mrowka.currentY++;
+                    } else {
+                        exitFlag = -1;
+                    }
+                    break;
+                case 3:
+                    mrowka.zwrot = 4;
+                    if (mrowka.currentX > 0) {
+                        mrowka.currentX--;
+                    } else {
+                        exitFlag = -1;
+                    }
+                    break;
+                case 4:
+                    mrowka.zwrot = 1;
+                    if (mrowka.currentY > 0) {
+                        mrowka.currentY--;
+                    } else {
+                        exitFlag = -1;
+                    }
+                    break;
+            }
+        }
+	switch(mrowka.zwrot){
+		case 1:
+			 pola[mrowka.currentX + (mrowka.currentY+1) * x] = 1 - pola[mrowka.currentX + (mrowka.currentY+1) * x];
+			 break;
+		case 2:
+			  pola[mrowka.currentX - 1 + mrowka.currentY * x] = 1 - pola[mrowka.currentX - 1 + mrowka.currentY * x];
+			  break;
+		case 3:
+			   pola[mrowka.currentX + (mrowka.currentY-1) * x] = 1 - pola[mrowka.currentX + (mrowka.currentY - 1) * x];
+			   break;
+		case 4:
+			    pola[mrowka.currentX + 1 + mrowka.currentY * x] = 1 - pola[mrowka.currentX + 1 + mrowka.currentY * x];
+			    break;
+	
+	}
+        
+        
+        dotychczasoweIteracje++;
+    }
+    free(pola);
+    return 0;
 }
 
--- mrowka.h
+++ mrowka.h
@@ -5,22 +5,23 @@
 #include <unistd.h>
 #include <wchar.h>
 #include <locale.h>
-// LINE_VERTICAL:â”‚
-// LINE_HORIZONTAL:â”€
-// LINE_DOWN_RIGHT:â”Ś
-// LINE_DOWN_LEFT:â”
-// LINE_UP_RIGHT:â””
-// LINE_UP_LEFT:â”
-// SQUARE_WHITE:
-// SQUARE_BLACK:â–
-// ARROW_NORTH_WHITE:â–ł
-// ARROW_NORTH_BLACK:â–˛
-// ARROW_EAST_WHITE:â–·
-// ARROW_EAST_BLACK:â–¶
-// ARROW_SOUTH_WHITE:â–˝
-// ARROW_SOUTH_BLACK:â–Ľ
-// ARROW_WEST_WHITE:â—
-// ARROW_WEST_BLACK:â—€
+#include <math.h>
+#define LINE_VERTICAL "│"
+#define LINE_HORIZONTAL "─"
+#define LINE_DOWN_RIGHT "┌"
+#define LINE_DOWN_LEFT "┐"
+#define LINE_UP_RIGHT "└"
+#define LINE_UP_LEFT "┘"
+#define SQUARE_WHITE " "
+#define SQUARE_BLACK "█"
+#define ARROW_NORTH_WHITE "△"
+#define ARROW_NORTH_BLACK "▲"
+#define ARROW_EAST_WHITE "▷"
+#define ARROW_EAST_BLACK "▶"
+#define ARROW_SOUTH_WHITE "▽"
+#define ARROW_SOUTH_BLACK "▼"
+#define ARROW_WEST_WHITE "◁"
+#define ARROW_WEST_BLACK "◀"
 
 typedef struct{
 int zwrot;
@@ -30,17 +31,65 @@ int currentY;
 
 }ant;
 
-typedef struct{
-int kolor;//1-czarny 0-bialy
 
-}pole;
+
 void printStart(int size){
 		printf(LINE_DOWN_RIGHT);
-		for(int k; k < size-2; k++){
+		for(int k=0; k < size-2; k++){
 			printf(LINE_HORIZONTAL);
 			}	
 		printf(LINE_DOWN_LEFT);
 	}
 	
+void createMap(char* string,int x, int y, ant mrowka, int pola[]){
+	memset(string,0,1600);
+	strcat(string,LINE_DOWN_RIGHT);
+                for(int k=0; k < x; k++){
+                        strcat(string,LINE_HORIZONTAL);
+                        }
+                strcat(string, LINE_DOWN_LEFT);
+		strcat(string,"\n");
+		for(int a = 0; a < y; a++){
+			strcat(string,LINE_VERTICAL);
+			for(int b = 0; b < x; b++){
+				
+				if(a == mrowka.currentY && b == mrowka.currentX){
+					switch(mrowka.zwrot){
+						case 1:
+							strcat(string,ARROW_NORTH_BLACK);
+							break;
+						 case 2:
+                                                        strcat(string,ARROW_EAST_BLACK);
+                                                        break;
+						 case 3:
+                                                        strcat(string,ARROW_SOUTH_BLACK);
+                                                        break;
+						 case 4:
+                                                        strcat(string,ARROW_WEST_BLACK);
+                                                        break;
+						default:
+							strcat(string,"XDD");
+							break;
+					}
+				} 
+				else if(pola[a*x+b] == 1)
+					strcat(string,SQUARE_BLACK);
+				else{
+					strcat(string,SQUARE_WHITE);
+				}
+			}
+			strcat(string,LINE_VERTICAL);
+			strcat(string,"\n");
+		}
+		 strcat(string,LINE_UP_RIGHT);
+                for(int k=0; k < x; k++){
+                        strcat(string,LINE_HORIZONTAL);
+                        }
+                strcat(string, LINE_UP_LEFT);
+		strcat(string, "\n\n");
+
+
+}
+
 
 
